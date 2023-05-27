f8520b76505ae85fd2859127f94ad2c3513c0d03
Viktor <konvit16@gmail.com>
2023-05-27 23:30:00 +0600

    Написал запрос на получение данных со всех страниц, реализовал первую версию базы данных

--- request/get_json_spell_list.py
+++ request/get_json_spell_list.py
@@ -1,49 +1,25 @@
-import requests
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 from request_get_json_data_spell import get_json_data_spell
+import re
 
 # URL страницы со списком заклинаний
 url = 'https://dnd.su/spells/'
+# Создаем список для хранения ссылок на заклинания
+spell_urls = []
 
-try:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-except:
-    print('Failed to retrieve page')
-    exit()
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+    page.goto(url)
+    spell_links = page.query_selector_all('.cards_list__item-wrapper')
+    for link in spell_links:
+        spell_url = link.get_attribute('href')
+        spell_urls.append(spell_url)
+        print(spell_url)
+    browser.close()
 
-# Находим основной контейнер
-container = soup.find('div', {'class': 'paper-1 card'})
 
-# Проверяем, что контейнер найден
-if container:
-    # Находим контейнер с карточками
-    card_wrapper = container.find('div', {'class': 'card-wrapper'})
-
-    # Проверяем, что контейнер с карточками найден
-    if card_wrapper:
-        # Находим список карточек
-        card_list = card_wrapper.find('ul', {'class': 'cards-list'})
-
-        # Проверяем, что список карточек найден
-        if card_list:
-            # Находим все элементы карточек
-            card_items = card_list.find_all('li', {'class': 'cards_list__item'})
-
-            # Проходимся по каждому элементу карточки
-            for card_item in card_items:
-                # Находим ссылку на заклинание
-                spell_link = card_item.find('a', {'class': 'cards_list__item-wrapper'})
-
-                # Проверяем, что ссылка найдена
-                if spell_link:
-                    spell_url = 'https://dnd.su' + spell_link['href']
-
-                    # Вызываем метод для обработки каждого заклинания
-                    get_json_data_spell(spell_url)
-        else:
-            print('Card list not found')
-    else:
-        print('Card wrapper not found')
-else:
-    print('Container not found')
+# Обрабатываем каждую ссылку на заклинание
+for spell_url in spell_urls:
+    spell_url = 'https://dnd.su/' + spell_url
+    get_json_data_spell(spell_url)
